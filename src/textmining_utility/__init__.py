8fed21feb2b4bc1fad52ce666562f56bd0182fb1
El Baff, Roxanne <roxanne.elbaff@dlr.de>
2022-07-11 22:37:03 +0200

    added resource

--- src/textmining_utility/__init__.py
+++ src/textmining_utility/__init__.py
@@ -1,6 +1,6 @@
 """textmining package that uses existing libraries"""
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from pkg_resources import resource_filename
 filepath = resource_filename(__name__, 'lexicons/')
