0d680d0fc9032fbc618909b3cf4cfc801df74839
El Baff, Roxanne <roxanne.elbaff@dlr.de>
2023-01-10 10:30:51 +0100

    fixed hedge output

--- src/nlpaf/annotator/instance/hedge_orchestrator.py
+++ src/nlpaf/annotator/instance/hedge_orchestrator.py
@@ -23,10 +23,7 @@ class HedgeOrchestrator(PipeOrchestrator):
                 out_arr.append(row)
         out_df: pd.DataFrame = pd.DataFrame(out_arr)
 
-        str_cols = [x for x in out_df.columns.tolist() if x.endswith("_words")]
-        num_cols = [x for x in out_df.columns.tolist() if x.endswith("_count") or x.endswith("_ratio") ]
-        out_df[[str_cols]].fillna("", inplace=True)
-        out_df[[num_cols]].fillna(0, inplace=True)
+        
         return out_df
 
 
